48c396ab61746cd92efadfd4230edb76146cfb59
oudia15 <oudia15@hotmail.fr>
2018-03-06 19:54:17 -0600

    BG color scheme changed

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Card.css';
 
 const Card = ({name, email, id}) => {
   return (
-    <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img src={`https://robohash.org/${id}?200x200`} alt='robot'/>
+    <div className='tc dib br3 pa3 ma2 grow bw2 shadow-5 bgGreen'>
+      <img src={`https://robohash.org/${Math.random.toString + id}?200x200`} alt='robot'/>
       <div>
         <h2> {name} </h2>
         <p> {email} </p>
